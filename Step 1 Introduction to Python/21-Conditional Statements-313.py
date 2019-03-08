b9a11025bb07bd06710b41f1cae7c853159aedb0
Olivier Brunet <45876462+obrunet@users.noreply.github.com>
2019-03-08 17:39:39 +0100

    new course added

---
+++ Step 1 Introduction to Python/21-Conditional Statements-313.py
@@ -0,0 +1,162 @@
+## 1. If Statements ##
+
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+free_apps_ratings = []
+for row in apps_data[1:]:
+    price = float(row[4])
+    rating = float(row[7])
+    if price == 0.0:
+        free_apps_ratings.append(rating)
+
+avg_rating_free = sum(free_apps_ratings) / len(free_apps_ratings)
+
+## 2. Booleans ##
+
+a_price = 0
+if a_price == 0:
+    print('This is free')
+if a_price == 1:
+    print('This is not free')
+
+## 3. The Average Rating of Non-free Apps ##
+
+# INITIAL CODE
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+non_free_apps_ratings = []
+for row in apps_data[1:]:
+    rating = float(row[7])
+    price = float(row[4])   
+    if price != 0.0:
+        non_free_apps_ratings.append(rating)
+    
+avg_rating_non_free = sum(non_free_apps_ratings) / len(non_free_apps_ratings)
+
+## 4. The Average Rating of Gaming Apps ##
+
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+non_games_ratings = []
+for row in apps_data[1:]: 
+    if row[11] != 'Games':
+        non_games_ratings.append(float(row[7]))
+                                 
+avg_rating_non_games = sum(non_games_ratings) / len(non_games_ratings)
+
+## 5. Multiple Conditions ##
+
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+free_games_ratings = []
+for row in apps_data[1:]:
+    rating, price, genre = float(row[7]), float(row[4]), row[11]
+    if price == 0.0 and genre == 'Games':
+        free_games_ratings.append(rating)
+        
+avg_rating_free_games = sum(free_games_ratings) / len(free_games_ratings)
+
+## 6. The or Operator ##
+
+# INITIAL CODE
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+games_social_ratings = []
+for row in apps_data[1:]:
+    rating = float(row[7])
+    genre = row[11]
+    if genre == 'Social Networking' or genre == 'Games':
+        games_social_ratings.append(rating)
+avg_games_social = sum(games_social_ratings) / len(games_social_ratings)
+
+## 7. Combining Logical Operators ##
+
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+nonfree_games_social_ratings = []
+for row in apps_data[1:]:
+    rating = float(row[7])
+    genre = row[11]
+    price = float(row[4])
+    
+    if (genre == 'Social Networking' or genre == 'Games') and price != 0:
+        nonfree_games_social_ratings.append(rating)
+        
+avg_non_free = sum(nonfree_games_social_ratings) / len(nonfree_games_social_ratings)
+
+## 8. Comparison Operators ##
+
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+ratings = []
+for row in apps_data[1:]:
+    rating = float(row[7])
+    price = float(row[4])
+    
+    if price > 9:
+        ratings.append(rating)
+        
+avg_rating = sum(ratings) / len(ratings)
+n_apps_more_9 = len(ratings)
+n_apps_less_9 = len(apps_data[1:]) - len(ratings)
+
+## 9. The else Clause ##
+
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+for app in apps_data[1:]:
+    price = float(app[4])
+    if price == 0.0:
+        app.append('free')
+    else:
+        app.append('non-free')
+
+apps_data[0].append('free_or_not')
+print(apps_data[:6])
+
+## 10. The elif Clause ##
+
+# INITIAL CODE
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+for app in apps_data[1:]:
+    price = float(app[4])
+
+    if price == 0:
+        app.append('free')
+    elif price > 0 and price < 20:
+        app.append('affordable')
+    elif price >= 20 and price < 50:
+        app.append('expensive')
+    elif price >= 50:
+        app.append('very expensive')
+        
+apps_data[0].append('price_label')
+print(apps_data[:6])
